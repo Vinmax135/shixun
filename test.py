8348ae7fd76ec6133affbee0ea4121f6e5f4a867
Vincent Maxillion <VINCENTMAXILLION@outlook.com>
2025-06-28 22:32:02 +0800

    dwa

--- test.py
+++ test.py
@@ -23,7 +23,8 @@ boxes, logits, phrases = predict(
 
 annotated_frame = annotate(image_source=image_source, boxes=boxes, logits=logits, phrases=phrases)
 
-W, H = image.size()
+print(image.size())
+"""
 boxes_px = boxes.copy()
 boxes_px[:, 0] *= W  # x_center
 boxes_px[:, 1] *= H  # y_center
@@ -34,7 +35,7 @@ x0 = boxes_px[:, 0] - boxes_px[:, 2] / 2
 y0 = boxes_px[:, 1] - boxes_px[:, 3] / 2
 x1 = boxes_px[:, 0] + boxes_px[:, 2] / 2
 y1 = boxes_px[:, 1] + boxes_px[:, 3] / 2
-
+"""
 print(f"{x0} {y0} {x1} {y1}")
 print("done")
 plt.imshow(annotated_frame)
