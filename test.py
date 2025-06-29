606ef53df153a0a96d50a3d6419f5af9ba6e5f81
Vincent Maxillion <VINCENTMAXILLION@outlook.com>
2025-06-29 14:50:49 +0800

    sad

--- test.py
+++ test.py
@@ -1,38 +1,14 @@
-query = input()
+text = input()
 
-from PIL import Image
-import torch
-from groundingdino.util.inference import load_model, load_image, predict, annotate
-import matplotlib.pyplot as plt
-import numpy as np
-from torchvision.ops import box_convert
+import spacy
 
-BOX_THRESHOLD = 0.35
-TEXT_THRESHOLD = 0.25
+nlp = spacy.load("en_core_web_sm")
 
-config_path = "../GroundingDINO/groundingdino/config/GroundingDINO_SwinT_OGC.py"
-weight_path = "../GroundingDINO/groundingdino_swint_ogc.pth"
-
-model = load_model(config_path, weight_path)
-
-image_source, image = load_image("./pre.png")
-
-boxes, logits, phrases = predict(
-            model=model,
-            image=image,
-            caption=query,
-            box_threshold=BOX_THRESHOLD,
-            text_threshold=TEXT_THRESHOLD
-        )
-
-annotated_frame = annotate(image_source=image_source, boxes=boxes, logits=logits, phrases=phrases)
-
-h, w, _ = image_source.shape
-boxes = boxes * torch.Tensor([w, h, w, h])
-xyxy = box_convert(boxes=boxes, in_fmt="cxcywh", out_fmt="xyxy").numpy()
-
-print(xyxy)
-print("done")
-plt.imshow(annotated_frame)
-plt.axis('off')
-plt.savefig("output.png", bbox_inches='tight')
+doc = nlp(text)
+for token in doc:
+    if token.dep_ in ("dobj", "pobj", "attr") and token.pos_ == "NOUN":
+        print(token.text)
+# fallback
+for chunk in doc.noun_chunks:
+    if chunk.root.pos_ == "NOUN":
+        print(chunk.text)
