84e506ae362f413474cb4839f7f9da10b297cb7e
Vincent Maxillion <VINCENTMAXILLION@outlook.com>
2025-06-29 14:56:31 +0800

    dwd

--- test.py
+++ test.py
@@ -6,5 +6,7 @@ nlp = spacy.load("en_core_web_sm")
 
 doc = nlp(text)
 for token in doc:
-    if token.dep_ in ("dobj", "pobj", "attr") and token.pos_ == "NOUN":
-        print(token.text)
+        if token.dep_ in ("dobj", "pobj", "attr") and token.pos_ == "NOUN":
+            modifiers = [child.text for child in token.children if child.dep_ == "amod"]
+            phrase = " ".join(modifiers + [token.text])
+            print(phrase.strip())
