c3f62bdb90d6006002525c8087630ec059e9e8ce
Vincent Maxillion <VINCENTMAXILLION@outlook.com>
2025-06-30 20:22:03 +0800

    daw

--- agents/smart_agent.py
+++ agents/smart_agent.py
@@ -12,7 +12,7 @@ from agents.base_agent import BaseAgent
 from crag_web_result_fetcher import WebSearchResult
 
 # Constants
-VISION_MODEL_NAME = "Salesforce/blip2-opt-2.7b"  # Fits 5-8GB range
+VISION_MODEL_NAME = "Salesforce/blip2-flan-t5-xl"  # Fits 5-8GB range
 BATCH_SIZE = 1
 BOX_THRESHOLD = 0.4
 TEXT_THRESHOLD = 0.25
